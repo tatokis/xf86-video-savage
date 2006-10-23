4f8660473ba079f98f097abd72ce2647c345d7cd
Alan Coopersmith <alan.coopersmith@sun.com>
2006-10-23 10:57:35 -0700

    Strip ^M's out of savage_driver.h

--- src/savage_driver.h
+++ src/savage_driver.h
@@ -444,7 +444,7 @@ typedef struct _Savage {
     __GLXvisualConfig*	pVisualConfigs;
     SAVAGEConfigPrivPtr 	pVisualConfigsPriv;
     SAVAGEDRIServerPrivatePtr DRIServerInfo;
-    ScreenWakeupHandlerProcPtr coreWakeupHandler;
+    ScreenWakeupHandlerProcPtr coreWakeupHandler;
     ScreenBlockHandlerProcPtr  coreBlockHandler;
 
 #if 0
